a63353990676b9ee033a98b26265b44d7dc16ca7
phoenixrhein <phoenixrhein@xovatec.de>
2023-08-21 23:06:12 +0200

    Added new scripts for user, category and bank account

---
+++ app/Console/Commands/UserDelete.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace de\xovatec\financeAnalyzer\Console\Commands;
+
+use de\xovatec\financeAnalyzer\Models\User;
+use Illuminate\Console\Command;
+
+class UserDelete extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fin:user-delete {userId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete a user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if ($this->confirm('Do you want to delete?') === false) {
+            return;
+        }
+        $userId = $this->argument('userId');
+        User::destroy($userId);
+        $this->info("Deleted user with id '{$userId}'");
+    }
+}
