254b36b584e8416bf9a2106fb8b96301114d0f14
phoenixrhein <phoenixrhein@xovatec.de>
2024-03-26 12:56:26 +0100

    test2

--- app/Console/Commands/Ruleset/RulesetAdd.php
+++ app/Console/Commands/Ruleset/RulesetAdd.php
@@ -36,6 +36,7 @@ class RulesetAdd extends Command
     public function handle(): void
     {
         //test1
+        //test2
         try {
             $rulesetData = $this->expressionParser->parse($this->argument('expression'));
             DB::beginTransaction();
