682fee101a825cbea31f2ead77a2f99105a3af4b
phoenixrhein <phoenixrhein@xovatec.de>
2024-05-13 23:26:02 +0200

    Rebuild user add

--- app/Console/Commands/User/UserAdd.php
+++ app/Console/Commands/User/UserAdd.php
@@ -2,40 +2,69 @@
 
 namespace de\xovatec\financeAnalyzer\Console\Commands\User;
 
+use de\xovatec\financeAnalyzer\Console\Commands\FinCommand;
 use de\xovatec\financeAnalyzer\Models\User;
-use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Validator;
+use function \Laravel\Prompts\text;
+use function \Laravel\Prompts\confirm;
 
-class UserAdd extends Command
+class UserAdd extends FinCommand
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'fin:user-add {email}';
+    protected $signature = 'fin:user-add';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Add a new user';
+    protected $description = 'cli.user.add.description';
 
     /**
-     * Execute the console command.
+     * @inheritDoc
      */
-    public function handle()
+    protected function process(): void
     {
-        $email = $this->argument('email');
-        $validator = Validator::make(['email' => $email], User::$rules);
-        if ($validator->fails()) {
-            $this->error($validator->errors()->first());
-            return;
-        }
+        $email = '';
+        do {
+            $email = text(
+                label: __('cli.user.add.input_email'),
+                default: $email
+            );
+            $validator = Validator::make(['email' => $email], User::$rules);
+            
+            $valid = true;
+            if ($validator->fails()) {
+                $valid = false;
+                $this->error($validator->errors()->first());
+            }
+
+            if ($valid && User::where('email', $email)->count()) {
+                $valid = false;
+                $this->error(__('cli.user.add.validate_error.duplicate_email'));
+            }
+
+            if (
+                $valid
+                && !confirm(
+                    label: __('cli.user.add.confirm'),
+                    yes: __('cli.base.button.create'),
+                    no: __('cli.base.button.abort')
+                )
+            ) {
+                return;
+            }
+                
+        } while (!$valid);
+        
         $newEntry = User::create([
             'email' => $email
         ]);
-        $this->info("user created with email: {$email} and id {$newEntry->id}");
+        
+        $this->info(__('cli.user.add.created', ['mail' => $email, 'id' => $newEntry->id]));
     }
 }
